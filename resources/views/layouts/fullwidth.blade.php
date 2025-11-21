452553660b15031dc9399b2d63741640d524291f
Novikov <ruslan.n@hypecharge.io>
2025-11-21 18:25:01 +0200

    Adjust submission table

---
+++ resources/views/layouts/fullwidth.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Actor Submission Form</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="bg-gray-50">
+    <div class="min-h-screen py-8">
+        <div class="mx-auto px-4">
+            @yield('content')
+        </div>
+    </div>
+</body>
+</html>
