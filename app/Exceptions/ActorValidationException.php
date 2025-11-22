4c7468c93d58af0ddbcbfca3db474646b41f1d18
Novikov <ruslan.n@hypecharge.io>
2025-11-22 13:29:59 +0200

    Cache API ai / PHPDoc / Pagination

--- app/Exceptions/ActorValidationException.php
+++ app/Exceptions/ActorValidationException.php
@@ -6,24 +6,10 @@ use Exception;
 
 class ActorValidationException extends Exception
 {
-    public function __construct(
-        string $message = 'Actor data validation failed',
-        private readonly array $missingFields = [],
-        int $code = 0,
-        ?\Throwable $previous = null
-    ) {
-        parent::__construct($message, $code, $previous);
-    }
-
-    public function getMissingFields(): array
-    {
-        return $this->missingFields;
-    }
-
     public static function missingRequiredFields(array $fields): static
     {
         $message = 'Missing required fields: ' . implode(', ', $fields);
 
-        return new static($message, $fields);
+        return new static($message);
     }
 }
